c1f139acbacad552ef43f3fe8ee80ee98a6a63ce
Ewokkkkk <ewok.mfk@gmail.com>
2021-01-28 15:05:54 +0900

    リスト機能完成

--- flask/main.py
+++ flask/main.py
@@ -53,9 +53,22 @@ def result():
                     data = request.args.get("name")
                     d_list = data.split(",")
                     data2 = request.args.get("ignore")
-                    d2_list = data.split(",")
+                    d2_list = data2.split(",")
                     name = data
-                    
+                    # sql = """SELECT recipe.title, recipe.image, recipe.recipe_id FROM recipe 
+                    # JOIN material_recipe 
+                    # ON recipe.recipe_id = material_recipe.recipe_id 
+                    # JOIN material 
+                    # ON material_recipe.material_id = material.material_id 
+                    # WHERE recipe.recipe_id NOT IN 
+                    # ( SELECT recipe_id
+                    # FROM material_recipe
+                    # WHERE material_id IN
+					# 	( SELECT material.material_id
+					# 	FROM material
+					# 	WHERE material_name LIKE %s)
+                    # ) AND material.material_name LIKE %s 
+                    # GROUP BY recipe.title"""
                     sql = """SELECT recipe.title, recipe.image, recipe.recipe_id FROM recipe 
                     JOIN material_recipe 
                     ON recipe.recipe_id = material_recipe.recipe_id 
@@ -65,12 +78,28 @@ def result():
                     ( SELECT recipe_id
                     FROM material_recipe
                     WHERE material_id IN
-						( SELECT material.material_id
-						FROM material
-						WHERE material_name LIKE %s)
-                    ) AND material.material_name LIKE %s 
-                    GROUP BY recipe.title"""
-                    cursor.execute(sql, (('%' + data2 + '%'), ('%' + data + '%')))
+					( SELECT material.material_id
+					FROM material
+					WHERE material_name LIKE %s """
+                    for i in range(len(d_list)-1):
+                        sql += """ OR material.material_name LIKE %s """
+
+                    sql += """ )
+                    ) AND material.material_name LIKE %s """
+
+                    for i in range(len(d2_list)-1):
+                        sql += """ OR material.material_name LIKE %s """
+
+                    for i in range(len(d_list)):
+                        d_list[i] = "%" + d_list[i] + "%"
+                    for i in range(len(d2_list)):
+                        d2_list[i] = "%" + d2_list[i] + "%"
+
+                    sql += """GROUP BY recipe.title"""
+
+
+                    print(d2_list + d_list)
+                    cursor.execute(sql, d2_list + d_list)
 
             # cursor.execute(sql, data)
         # Select結果を取り出す 
